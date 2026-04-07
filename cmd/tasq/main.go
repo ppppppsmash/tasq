8d0cefd3db668b01e009ee0cdf0b27568539a638
a.kurosawa <pppppp.work@gmail.com>
2026-04-07 15:16:12 +0900

    feat: add main entrypoint

---
+++ cmd/tasq/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	// TODO: Socket Mode接続を実装
+	fmt.Println("tasq starting...")
+	return nil
+}
